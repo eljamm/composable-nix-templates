906f09626432753822ee0cdaa4c0bf507ee54778
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-14 11:34:04 +0200

    templates: add default

---
+++ templates/default/default.nix
@@ -0,0 +1,46 @@
+let
+  flake-inputs = import (
+    fetchTarball "https://github.com/fricklerhandwerk/flake-inputs/tarball/4.1.0"
+  );
+  inherit (flake-inputs)
+    import-flake
+    ;
+in
+{
+  self ? import-flake {
+    src = ./.;
+  },
+  sources ? self.inputs,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs {
+    config = { };
+    overlays = [ ];
+    inherit system;
+  },
+  lib ? import "${sources.nixpkgs}/lib",
+}:
+let
+  args = {
+    inherit
+      lib
+      pkgs
+      system
+      sources
+      ;
+  };
+
+  formatter = import ./dev/formatter.nix args;
+
+  default = {
+    inherit
+      formatter
+      ;
+
+    shell = pkgs.mkShellNoCC {
+      packages = [
+        formatter
+      ];
+    };
+  };
+in
+default // args
