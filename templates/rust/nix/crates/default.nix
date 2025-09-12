2045281667e4c42e1028b38eb89720e3d4374127
eljamm <fedi.jamoussi@protonmail.ch>
2025-09-12 17:52:27 +0200

    refactor(rust): crate scope

--- templates/rust/nix/crates/default.nix
+++ templates/rust/nix/crates/default.nix
@@ -4,24 +4,20 @@
   inputs,
   ...
 }:
-let
-  crates = lib.makeScope pkgs.newScope (
-    self:
-    let
-      callCrate = self.newScope rec {
-        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
+lib.makeScope pkgs.newScope (
+  self:
+  let
+    callCrate = self.newScope rec {
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
 
-        # src -> { `pname`, `version` }
-        crateInfo = src: craneLib.crateNameFromCargoToml { cargoToml = "${src}/Cargo.toml"; };
+      # src -> { `pname`, `version` }
+      crateInfo = src: craneLib.crateNameFromCargoToml { cargoToml = "${src}/Cargo.toml"; };
 
-        # use mold linker
-        stdenv = p: p.stdenvAdapters.useMoldLinker self.clangStdenv;
-      };
-    in
-    with self;
-    {
-      default = callCrate ./hello.nix { };
-    }
-  );
-in
-crates
+      # use mold linker
+      stdenv = p: p.stdenvAdapters.useMoldLinker self.clangStdenv;
+    };
+  in
+  {
+    default = callCrate ./hello.nix { };
+  }
+)
